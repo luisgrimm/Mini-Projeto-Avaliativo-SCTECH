ed3bb0e4ef1f49ab778607a4caa400eedc19fa91
Luis Grimm <luisgrimm36@gmail.com>
2026-05-20 23:53:29 -0300

    feat: criação do método listarVagas

--- skillmatch.js
+++ skillmatch.js
@@ -37,43 +37,53 @@ class Vagas {
       },
     ];
   }
+
+  listarVagas(){
+    let listagem = "";
+    let numero = 1
+    for(let item of this.vagas){
+      listagem += numero++ + " " + "Empresa: " + item.empresa + "\n" +
+      "Cargo: " + item.cargo + "\n" +
+      "Habilidades exigidas: " + item.requisitos.join(", ") + "\n\n"
+    }
+    alert(listagem)
+  }
 }
+
 class AutomatizadorDeVagas extends Vagas {
   constructor() {
     super();
     this.candidatoAhSerVerificado = new Candidato();
   }
-  calcularCompatibilidade(numero) {
-    let indice = numero;
+
+  calcularCompatibilidade(indice) {
     let vagaAhSerVerificada = this.vagas[indice];
     let requisitosAtendidos = vagaAhSerVerificada.requisitos.filter((item) => {
       return this.candidatoAhSerVerificado.candidato.habilidades.includes(item);
     });
-    let calcularPorcentagem =
-      (requisitosAtendidos.length / vagaAhSerVerificada.requisitos.length) *
-      100;
+    let calcularPorcentagem = (requisitosAtendidos.length / vagaAhSerVerificada.requisitos.length) * 100;
     return calcularPorcentagem;
   }
 
   vagaComMaiorCompatibilidade() {
     let porcentagens = [];
-    for(let indice in this.vagas){
-      porcentagens.push(this.calcularCompatibilidade(indice))
+    for (let indice in this.vagas) {
+      porcentagens.push(this.calcularCompatibilidade(indice));
     }
     let maior = porcentagens.reduce((acc, numero) => {
       return numero > acc ? numero : acc;
-    })
+    });
 
-    let indices = porcentagens.map((numero, indice) => numero === maior ? indice : -1).filter(indice => indice !== -1)
+    let indices = porcentagens
+      .map((numero, indice) => (numero === maior ? indice : -1))
+      .filter((indice) => indice !== -1);
 
-    for(let item in indices){
-      alert(JSON.stringify(this.vagas[item]))
+    let aparecer = ""
+    for (let item in indices) {
+      aparecer += "Vaga mais compatível: " + "\n" + this.vagas[item].empresa + " - " + this.vagas[item].cargo + 
+      "\n" + "Compatibilidade: " + this.calcularCompatibilidade(item) + "%" + "\n\n";
     }
-
+    alert(aparecer)
   }
+
 }
-
-let av = new AutomatizadorDeVagas()
-
-av.vagaComMaiorCompatibilidade()
-
