84e5eadd0b9786ac4605bae6c418a44dfaf23748
Luis Grimm <luisgrimm36@gmail.com>
2026-05-23 00:41:56 -0300

    feat: finalização do método vagaAnalisada

--- skillmatch.js
+++ skillmatch.js
@@ -1,9 +1,9 @@
 class Candidato {
   constructor() {
     this.candidato = {
-      nome: "Ana",
+      nome: "Luis",
       area: "Front-End",
-      habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "Kanban"],
+      habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "Funções"],
       experienciaMeses: 3,
     };
   }
@@ -35,22 +35,33 @@ class Vagas {
         salario: 3000,
         modalidade: "Presencial",
       },
+      {
+        id: 4,
+        empresa: "DevWave",
+        cargo: "Desenvolvedor Front-End Trainee",
+        requisitos: ["Funções", "JavaScript"],
+        salario: 2200,
+        modalidade: "Remoto",
+      },
+      {
+        id: 5,
+        empresa: "ByteTech",
+        cargo: "Desenvolvedor JavaScript Júnior",
+        requisitos: ["JavaScript", "Funções", "Objetos", "GitHub"],
+        salario: 3200,
+        modalidade: "Híbrido",
+      }
     ];
   }
 
   async listarVagas(callback) {
     let listagem = "";
     for (let item of this.vagas) {
-      listagem +=
-        item.id + " - " +
-        "Empresa: " +
-        item.empresa +
+      listagem += item.id + " - " + "Empresa: " + item.empresa +
         "\n" +
-        "Cargo: " +
-        item.cargo +
+        "Cargo: " + item.cargo +
         "\n" +
-        "Habilidades exigidas: " +
-        item.requisitos.join(", ") +
+        "Habilidades exigidas: " + item.requisitos.join(", ") +
         "\n\n";
     }
 
@@ -65,11 +76,6 @@ class Vagas {
 
     let contar = contadorDeVagas();
 
-    alert("Listando vagas...");
-    for(let item in this.vagas){
-       alert(contar() + " Vagas encontradas"); 
-    }
-
     let promise = new Promise((resolve, reject) => {
       setTimeout(() => {
         let sucesso = true;
@@ -78,13 +84,18 @@ class Vagas {
         } else {
           reject("Ocorreu um erro na operação");
         }
-      }, 2000);
+      }, 1000);
     });
+
+    alert("Listando vagas...");
+    for (let item in this.vagas) {
+    alert(contar() + " Vagas encontradas");
+    }
 
     try {
       let resultado = await promise;
       let opcao = Number(prompt("Selecione a vaga que você quer analisar: " + "\n\n" + resultado));
-      callback(opcao)
+      callback(opcao);
     } catch (erro) {
       alert(erro);
     }
@@ -101,21 +112,17 @@ class AutomatizadorDeVagas extends Vagas {
     alert(
       "Seu curriculo:" +
         "\n" +
-        "Nome: " +
-        this.candidatoAhSerVerificado.candidato.nome +
+        "Nome: " + this.candidatoAhSerVerificado.candidato.nome +
         "\n" +
-        "Area: " +
-        this.candidatoAhSerVerificado.candidato.area +
+        "Area: " + this.candidatoAhSerVerificado.candidato.area +
         "\n" +
-        "Habilidades: " +
-        this.candidatoAhSerVerificado.candidato.habilidades.join(", ") +
+        "Habilidades: " + this.candidatoAhSerVerificado.candidato.habilidades.join(", ") +
         "\n" +
-        "Experiencia em meses: " +
-        this.candidatoAhSerVerificado.candidato.experienciaMeses,
+        "Experiencia em meses: " + this.candidatoAhSerVerificado.candidato.experienciaMeses
     );
   }
 
-  vagaAnalisada(numero){
+  vagaAnalisada(numero) {
     let indice = numero - 1;
     let vagaAnalisada = this.vagas[indice];
     let porcentagem = this.calcularCompatibilidade(indice);
@@ -124,19 +131,67 @@ class AutomatizadorDeVagas extends Vagas {
       return this.candidatoAhSerVerificado.candidato.habilidades.includes(item);
     });
 
+    let requisitosNaoAtendidos = vagaAnalisada.requisitos.filter((item) => {
+      return !this.candidatoAhSerVerificado.candidato.habilidades.includes(
+        item,
+      );
+    });
 
-    let classificacao = "";
-    if(porcentagem >= 80){
-      classificacao = "Alta compatibilidade"
-    } else if(porcentagem >= 50){
-      classificacao = "Média compatibilidade"
+    let habilidadesCompativeis = "";
+    let habilidadesNaoCompativeis = "";
+
+    if (requisitosAtendidos.length === 0) {
+      habilidadesCompativeis = "Nenhuma habilidade compativel";
     } else {
-      classificacao = "Baixa compatibilidade"
+      habilidadesCompativeis = requisitosAtendidos.join(", ");
     }
 
+    if (requisitosNaoAtendidos.length === 0) {
+      habilidadesNaoCompativeis = "Nenhuma habilidade faltante";
+    } else {
+      habilidadesNaoCompativeis = requisitosNaoAtendidos.join(", ");
+    }
 
-    alert("Empresa: " + vagaAnalisada.empresa + "\n" + "Cargo: " + vagaAnalisada.cargo + "\n" + "Compatibilidade: " + porcentagem + "%" + "\n" + "Classificação: " + classificacao)
+    let classificacao = "";
+    if (porcentagem >= 80) {
+      classificacao = "Alta compatibilidade";
+    } else if (porcentagem >= 50) {
+      classificacao = "Média compatibilidade";
+    } else {
+      classificacao = "Baixa compatibilidade";
+    }
 
+    let opcao = Number(prompt("Empresa: " + vagaAnalisada.empresa +
+        "\n" +
+        "Cargo: " + vagaAnalisada.cargo +
+        "\n" +
+        "Compatibilidade: " + porcentagem + "%" +
+        "\n" +
+        "Habilidades encontradas: " + habilidadesCompativeis +
+        "\n" +
+        "Habilidades faltantes: " + habilidadesNaoCompativeis +
+        "\n" +
+        "Classificação: " + classificacao + 
+        "\n\n" + 
+        "1 - Se deseja gerar uma recomendação de estudo \n2 - Se deseja listar as habilidades faltantes"
+    ));
+
+    switch(opcao){
+      case 1:
+        if(requisitosNaoAtendidos.length === 0) {
+          alert("Parabens!! \nNão há o que estudar, todas suas habilidades são compativeis com a vaga")
+        } else {
+          alert(`Recomendação de estudo: \nPriorize estudar ${habilidadesNaoCompativeis}, pois esses conteúdos aparecem na vaga analisada.`)
+        }
+        break
+      case 2:
+        if(requisitosNaoAtendidos.length === 0) {
+          alert("Parabens!! \nTodas suas habilidades são compativeis com a vaga")
+        } else {
+          alert(`Para a vaga da ${vagaAnalisada.empresa}, faltam:\n- ${requisitosNaoAtendidos.join("\n-")}`)
+        }
+        break
+    }
   }
 
   calcularCompatibilidade(indice) {
@@ -144,10 +199,8 @@ class AutomatizadorDeVagas extends Vagas {
     let requisitosAtendidos = vagaAhSerVerificada.requisitos.filter((item) => {
       return this.candidatoAhSerVerificado.candidato.habilidades.includes(item);
     });
-    let calcularPorcentagem =
-      (requisitosAtendidos.length / vagaAhSerVerificada.requisitos.length) *
-      100;
-    return calcularPorcentagem;
+    let calcularPorcentagem = (requisitosAtendidos.length / vagaAhSerVerificada.requisitos.length) * 100;
+    return Number(calcularPorcentagem.toFixed(1));
   }
 
   vagaComMaiorCompatibilidade() {
@@ -159,23 +212,15 @@ class AutomatizadorDeVagas extends Vagas {
       return numero > acc ? numero : acc;
     });
 
-    let indices = porcentagens
-      .map((numero, indice) => (numero === maior ? indice : -1))
-      .filter((indice) => indice !== -1);
+    let indices = porcentagens.map((numero, indice) => (numero === maior ? indice : -1)).filter((indice) => indice !== -1);
 
     let aparecer = "";
-    for (let item in indices) {
-      aparecer +=
-        "Vaga mais compatível: " +
-        "\n" +
-        this.vagas[item].empresa +
-        " - " +
-        this.vagas[item].cargo +
-        "\n" +
-        "Compatibilidade: " +
-        this.calcularCompatibilidade(item) +
-        "%" +
-        "\n\n";
+    for (let item of indices) {
+      aparecer +="Vaga mais compatível: " + 
+      "\n" + 
+      this.vagas[item].empresa + " - " + this.vagas[item].cargo + 
+      "\n" + 
+      "Compatibilidade: " + this.calcularCompatibilidade(item) + "%" + "\n\n";
     }
     alert(aparecer);
   }
@@ -185,18 +230,21 @@ class AutomatizadorDeVagas extends Vagas {
     do {
       opcao = Number(
         prompt(
-          "-----Bem Vindo----- \n -----Escolha----- \n 1 - Analisar Vagas \n 2 - Ver seu curriculo \n 0 - Sair",
+          "-----Bem Vindo----- \n -----Escolha----- \n 1 - Analisar Vagas \n 2 - Ver seu curriculo \n 3 - Vagas com maior compatibilidade \n 0 - Sair",
         ),
       );
       switch (opcao) {
         case 1:
-          await skillmatch.listarVagas((opcao) => {
-            skillmatch.vagaAnalisada(opcao)
+          await this.listarVagas((opcao) => {
+            this.vagaAnalisada(opcao);
           });
           break;
         case 2:
-          skillmatch.verCurriculo();
+          this.verCurriculo();
           break;
+        case 3:
+          this.vagaComMaiorCompatibilidade();
+          break
         case 0:
           alert("Saindo...");
           break;
